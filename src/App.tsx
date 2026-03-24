f70505ab5009fd20039fb6094724f128aa6097f7
Welinson <welinsonmarlon15@gmail.com>
2026-03-23 22:04:06 -0300

    route update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { AppProviders } from "@/providers/AppProviders";
-import { AppRoutes } from "@/routes";
-
+import { AppProviders } from "./providers/AppProviders";
+import { SecurityGuard } from "./modules/common/components/SecurityGuard";
+import { PwaGuard } from "./modules/common/components/PwaGuard";
+import { CookieConsent } from "./modules/common/components/CookieConsent";
+import { InstallPWA } from "./modules/common/components/InstallPWA";
+import { ConnectionStatus } from "./modules/common/components/ConnectionStatus";
+import { AppRoutes } from "./routes/AppRoutes";
 const App = () => {
   return (
     <AppProviders>
-      <AppRoutes />
+      <SecurityGuard>
+        <PwaGuard>
+          <CookieConsent />
+          <InstallPWA />
+          <ConnectionStatus />
+          <AppRoutes />
+        </PwaGuard>
+      </SecurityGuard>
     </AppProviders>
   );
 };
--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -5,9 +5,12 @@ import { SaaSProvider } from "@/core/context/SaaSContext";
 import { RestaurantProvider } from "@/core/context/RestaurantContext";
 import { PwaProvider } from "@/core/context/PwaContext";
 import { UIProvider } from "@/core/context/UIContext";
+
 import { InventoryProvider } from "@/core/context/InventoryContext";
 import { FinanceProvider } from "@/core/context/FinanceContext";
 import { OrderProvider } from "@/core/context/OrderContext";
+import { MenuProvider } from "@/core/context/MenuContext";
+import { StaffProvider } from "@/core/context/StaffContext";
 
 export const AppProviders = ({ children }: PropsWithChildren) => {
   return (
@@ -16,15 +19,22 @@ export const AppProviders = ({ children }: PropsWithChildren) => {
         <SaaSProvider>
           <PwaProvider>
             <RestaurantProvider>
-                <InventoryProvider>
-                    <FinanceProvider>
-                        <OrderProvider>
- {children}
-                        </OrderProvider>
- 
-                    </FinanceProvider>
-                </InventoryProvider>
-            
+
+              {/* Providers do sistema do restaurante */}
+              <MenuProvider>
+                <OrderProvider>
+                  <StaffProvider>
+                    <InventoryProvider>
+                      <FinanceProvider>
+
+                        {children}
+
+                      </FinanceProvider>
+                    </InventoryProvider>
+                  </StaffProvider>
+                </OrderProvider>
+              </MenuProvider>
+
             </RestaurantProvider>
           </PwaProvider>
         </SaaSProvider>
